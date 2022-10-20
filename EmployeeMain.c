440ce9ba60d5af9deff7e81aefaf3e59bbe876d9
djRemskii <jeremyong@cpp.edu>
2022-10-19 20:06:46 -0700

    finished creating testing code for EmployeeMain.c

--- EmployeeMain.c
+++ EmployeeMain.c
@@ -16,7 +16,7 @@ int main(void){
     PtrToEmployee matchPtr;  //Declaration 
     matchPtr = searchEmployeeByNumber(EmployeeTable, EmployeeTableEntries, 1045); 
 
- //Example not found 
+    //Example not found 
     if (matchPtr != NULL) 
         printf("Employee ID 1045 is in record %d\n", matchPtr - EmployeeTable); 
     else 
@@ -31,6 +31,40 @@ int main(void){
     else 
         printf("Employee Tony Bobcat is NOT found in the record\n"); 
 
+
+
+
+    //Example Phone Number Found
+    matchPtr = searchEmployeeByPhoneNumber(EmployeeTable, EmployeeTableEntries, "909-555-2134"); 
+    if (matchPtr != NULL) 
+        printf("Employee phone number 909-555-2134 is in record %d\n", matchPtr - EmployeeTable); 
+    else 
+        printf("Employee phone Number 909-555-2134 is NOT found in the record\n"); 
+
+
+    //Example Phone Number Not Found
+    matchPtr = searchEmployeeByPhoneNumber(EmployeeTable, EmployeeTableEntries, "123-456-7890"); 
+    if (matchPtr != NULL) 
+        printf("Employee phone number 123-456-7890 is in record %d\n", matchPtr - EmployeeTable); 
+    else 
+        printf("Employee phone Number 123-456-7890 is NOT found in the record\n"); 
+
+
+
+    //Example Salary Found
+    matchPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 7.80); 
+    if (matchPtr != NULL) 
+        printf("Employee with salary of 7.80 is in record %d\n", matchPtr - EmployeeTable); 
+    else 
+        printf("Employee with salary of 7.80 is NOT found in the record\n"); 
+
+
+    //Example Salary Not Found
+    matchPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 9.99); 
+    if (matchPtr != NULL) 
+        printf("Employee with salary of 9.99 is in record %d\n", matchPtr - EmployeeTable); 
+    else 
+        printf("Employee with salary of 9.99 is NOT found in the record\n"); 
 
     return EXIT_SUCCESS;
 }
